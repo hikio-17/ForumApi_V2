980aab57e93d334f3a724774b3868c58c32615d5
hikio-17 <fajri010217@gmail.com>
2023-03-20 19:12:56 -0700

    add like comment use case test

---
+++ src/Applications/use_case/_test/AddLikeCommentUseCase.test.js
@@ -0,0 +1,39 @@
+const CommentRepository = require('../../../Domains/comments/CommentRepository');
+const AddLikeCommentUseCase = require('../AddLikeComentUseCase');
+
+describe('AddLikeCommentUseCase', () => {
+  it('should throw error if commentId and threadId not string', async () => {
+    // Arrange
+    const commentId = [];
+    const credentialId = true;
+
+    const commentRepository = new CommentRepository();
+    const addLikeComentUseCase = new AddLikeCommentUseCase(commentRepository);
+
+    // Action & Assert
+    await expect(addLikeComentUseCase.execute(commentId, credentialId)).rejects.toThrowError('ADD_LIKE_COMMENT_USE_CASE_NOT_MEET_DATA_TYPE_SPESIFICATION');
+  });
+
+  it('should orchestrating the add like comment action correctly', async () => {
+    // Arrange
+    const commentId = 'comment-1234';
+    const credentialId = 'user-1234';
+
+    // creating dependency of use case
+    const mockCommentRepository = new CommentRepository();
+
+    // mocking needed function
+    mockCommentRepository.likesComents = jest.fn(() => Promise.resolve());
+
+    // creating use case instance
+    const getAddLikeCommentUseCase = new AddLikeCommentUseCase({
+      commentRepository: mockCommentRepository,
+    });
+
+    // Action
+    await getAddLikeCommentUseCase.execute(commentId, credentialId);
+
+    // Assert
+    expect(mockCommentRepository.likesComents).toBeCalledWith(commentId, credentialId);
+  });
+});
--- src/Domains/comments/CommentRepository.js
+++ src/Domains/comments/CommentRepository.js
@@ -15,6 +15,10 @@ class CommentRepository {
     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 
+  async likesComents(commentId) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
   async deleteCommentById(id) {
     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
